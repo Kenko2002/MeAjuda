a2f6ca6bd157d7eee59a661d7bb955ea806d0dba
Renzo Fraga <kenkomarinho@gmail.com>
2026-03-18 17:19:27 -0300

    MeAjuda2

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import login_view, logout_view, home_view,profile_view
 
 urlpatterns = [
-    path('login/', login_view, name='login'),
+    path("", login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('perfil/', profile_view, name='perfil'),
     path('', home_view, name='home'),
